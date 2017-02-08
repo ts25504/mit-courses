bdd6d24c55b37998aff19304af170d3873dc5cf8
ts25504 <ts25504@gmail.com>
2017-02-08 22:44:53 +0800

    Add 6.824-2016

---
+++ 6.824-2016/src/mapreduce/common_map.go
@@ -0,0 +1,95 @@
+package mapreduce
+
+import (
+	"hash/fnv"
+	"os"
+	"encoding/json"
+	"log"
+	"fmt"
+)
+
+// doMap does the job of a map worker: it reads one of the input files
+// (inFile), calls the user-defined map function (mapF) for that file's
+// contents, and partitions the output into nReduce intermediate files.
+func doMap(
+	jobName string, // the name of the MapReduce job
+	mapTaskNumber int, // which map task this is
+	inFile string,
+	nReduce int, // the number of reduce task that will be run ("R" in the paper)
+	mapF func(file string, contents string) []KeyValue,
+) {
+	fmt.Printf("Map: read %s\n", inFile)
+	file, err := os.Open(inFile)
+	if err != nil {
+		log.Fatal("Map: ", err)
+	}
+
+	info, err := file.Stat()
+	if err != nil {
+		log.Fatal("Map: ", err)
+	}
+
+	buffer := make([]byte, info.Size())
+	_, err = file.Read(buffer)
+	if err != nil {
+		log.Fatal("Map: ", err)
+	}
+
+	file.Close()
+
+	kvs := mapF(inFile, string(buffer))
+
+	for i := 0; i < nReduce; i++ {
+		fileName := reduceName(jobName, mapTaskNumber, i)
+		fmt.Printf("Map: write %s\n", fileName)
+		file, err = os.Create(fileName)
+		if err != nil {
+			log.Fatal("Map: ", err)
+		}
+
+		enc := json.NewEncoder(file)
+		for _, kv := range kvs {
+			if ihash(kv.Key) % uint32(nReduce) == uint32(i) {
+				err = enc.Encode(&kv)
+				if err != nil {
+					log.Fatal("Map: ", err)
+				}
+			}
+		}
+
+		file.Close()
+	}
+
+	// TODO:
+	// You will need to write this function.
+	// You can find the filename for this map task's input to reduce task number
+	// r using reduceName(jobName, mapTaskNumber, r). The ihash function (given
+	// below doMap) should be used to decide which file a given key belongs into.
+	//
+	// The intermediate output of a map task is stored in the file
+	// system as multiple files whose name indicates which map task produced
+	// them, as well as which reduce task they are for. Coming up with a
+	// scheme for how to store the key/value pairs on disk can be tricky,
+	// especially when taking into account that both keys and values could
+	// contain newlines, quotes, and any other character you can think of.
+	//
+	// One format often used for serializing data to a byte stream that the
+	// other end can correctly reconstruct is JSON. You are not required to
+	// use JSON, but as the output of the reduce tasks *must* be JSON,
+	// familiarizing yourself with it here may prove useful. You can write
+	// out a data structure as a JSON string to a file using the commented
+	// code below. The corresponding decoding functions can be found in
+	// common_reduce.go.
+	//
+	//   enc := json.NewEncoder(file)
+	//   for _, kv := ... {
+	//     err := enc.Encode(&kv)
+	//
+	// Remember to close the file after you have written all the values!
+}
+
+func ihash(s string) uint32 {
+	h := fnv.New32a()
+	h.Write([]byte(s))
+	return h.Sum32()
+}
---
+++ 6.824-2016/src/mapreduce/common_reduce.go
@@ -0,0 +1,81 @@
+package mapreduce
+
+import (
+	"os"
+	"encoding/json"
+	"log"
+	"fmt"
+)
+
+// doReduce does the job of a reduce worker: it reads the intermediate
+// key/value pairs (produced by the map phase) for this task, sorts the
+// intermediate key/value pairs by key, calls the user-defined reduce function
+// (reduceF) for each key, and writes the output to disk.
+func doReduce(
+	jobName string, // the name of the whole MapReduce job
+	reduceTaskNumber int, // which reduce task this is
+	nMap int, // the number of map tasks that were run ("M" in the paper)
+	reduceF func(key string, values []string) string,
+) {
+	kvs := make(map[string][]string)
+	for i := 0; i < nMap; i++ {
+		fileName := reduceName(jobName, i, reduceTaskNumber)
+		fmt.Printf("Reduce: read %s\n", fileName)
+		file, err := os.Open(fileName)
+		if err != nil {
+			log.Fatal("Reduce: ", err)
+		}
+
+		dec := json.NewDecoder(file)
+		for {
+			var kv KeyValue
+			err := dec.Decode(&kv)
+			if err != nil {
+				break
+			}
+
+			kvs[kv.Key] = append(kvs[kv.Key], kv.Value)
+		}
+
+		file.Close()
+	}
+
+	fileName := mergeName(jobName, reduceTaskNumber)
+	fmt.Printf("Reduce: write %s\n", fileName)
+	file, err := os.Create(fileName)
+	if err != nil {
+		log.Fatal("Reduce: ", err)
+	}
+
+	enc := json.NewEncoder(file)
+	for key := range kvs {
+		err = enc.Encode(KeyValue{key, reduceF(key, kvs[key])})
+		if err != nil {
+			log.Fatal("Reduce: ", err)
+		}
+	}
+
+	file.Close()
+
+	// TODO:
+	// You will need to write this function.
+	// You can find the intermediate file for this reduce task from map task number
+	// m using reduceName(jobName, m, reduceTaskNumber).
+	// Remember that you've encoded the values in the intermediate files, so you
+	// will need to decode them. If you chose to use JSON, you can read out
+	// multiple decoded values by creating a decoder, and then repeatedly calling
+	// .Decode() on it until Decode() returns an error.
+	//
+	// You should write the reduced output in as JSON encoded KeyValue
+	// objects to a file named mergeName(jobName, reduceTaskNumber). We require
+	// you to use JSON here because that is what the merger than combines the
+	// output from all the reduce tasks expects. There is nothing "special" about
+	// JSON -- it is just the marshalling format we chose to use. It will look
+	// something like this:
+	//
+	// enc := json.NewEncoder(mergeFile)
+	// for key in ... {
+	// 	enc.Encode(KeyValue{key, reduceF(...)})
+	// }
+	// file.Close()
+}
